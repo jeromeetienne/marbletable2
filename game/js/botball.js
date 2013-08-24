4b291dbd2eb64bea2e963d50ad9eb70737a82058
Jerome Etienne <jerome.etienne@gmail.com>
2013-08-24 19:04:34 +0200

    more work

--- game/js/botball.js
+++ game/js/botball.js
@@ -1,37 +1,49 @@
-var BotBall0	= function(opts){
+var BotBall2	= function(opts){
+	//////////////////////////////////////////////////////////////////////////////////
+	//		options								//
+	//////////////////////////////////////////////////////////////////////////////////
+	
 	opts	= opts	|| {}
 	var ballAttraction	= opts.ballAttraction !== undefined ? opts.ballAttraction : 0.1
-	var texture	= opts.texture || cache.getSet('texture.botball', function(){
+	var radius		= opts.radius !== undefined ? opts.radius : 1.5*GAME.tileW
+	var material		= opts.material || cache.getSet('botball.material', function(){
 		var texture	= THREE.ImageUtils.loadTexture('images/planets/mars_1k_color.jpg');
-		var texture	= THREE.ImageUtils.loadTexture('images/sports/Footballrealisticsmall.jpgf0067587-1f18-410c-8650-63532bd9370fLarge.jpg')
-		var texture	= THREE.ImageUtils.loadTexture('images/sports/Footballballfree.jpg59a2a1dc-64c8-4bc3-83ef-1257c9147fd1Large.jpg')
-		var texture	= THREE.ImageUtils.loadTexture('images/sports/Basketball texture small.jpgb0270bdb-8751-473f-86c6-0c72ca7480b9Large.jpg')
-		return texture
+		var material	= new THREE.MeshPhongMaterial({
+			map	: texture,
+		})
+		return material
 	})
+	var nLives		= opts.nLives !== undefined ? opts.nLives : 1
 	
-	// handle updateFcts for sounds
+
+	//////////////////////////////////////////////////////////////////////////////////
+	//		update function							//
+	//////////////////////////////////////////////////////////////////////////////////
+	
 	var updateFcts	= [];
 	this.update	= function(delta, now){
 		updateFcts.forEach(function(updateFct){
 			updateFct(delta, now)
 		})
 	}
-	// create THREE.Object3d
-	var radius	= 1.5 * GAME.tileW
+	
+	//////////////////////////////////////////////////////////////////////////////////
+	//		create THREE.Object3D						//
+	//////////////////////////////////////////////////////////////////////////////////
+	
 	var geometry	= new THREE.SphereGeometry(radius, 32, 32)
-	var material	= new THREE.MeshPhongMaterial({
-		map	: texture,
-		// bumpMap	: texture,
-		// bumpScale: 0.05,
-	})
 	var object3d	= new THREE.Mesh(geometry, material)
 	this.object3d	= object3d
 	object3d.name	= (object3d.name || ' ') + 'ball ';
-
 	object3d.receiveShadow	= true
 	object3d.castShadow	= true
 	scene.add( object3d )
 
+
+	//////////////////////////////////////////////////////////////////////////////////
+	//		add physics							//
+	//////////////////////////////////////////////////////////////////////////////////
+	
 	var bodyx	= new THREEx.CannonBody({
 		mesh	: object3d,
 		material: pMaterialEnemy,
@@ -43,8 +55,7 @@ var BotBall0	= function(opts){
 
 	// setup origin	
 	if( opts.position ){
-		var position	= opts.position.clone()
-		//console.log('position', position)
+		var origin	= opts.position.clone()
 	}else{
 		// count the number of body of this type - used to fix startPosition
 		var bodyCounter	= 0
@@ -53,15 +64,16 @@ var BotBall0	= function(opts){
 			if( !isBall )	return
 			bodyCounter++
 		})
-		var position	= new THREE.Vector3()
-		position.set(-6*GAME.tileW, 6*GAME.tileW, -10*GAME.tileW)
-		position.z	*= Math.floor(bodyCounter % 2) === 1 ? 1 : -1
-		position.x	+= (Math.random()-0.5)*GAME.tileW*10;
-		position.y	+= bodyCounter * radius*2 * 1.5
-		position.z	+= (Math.random()-0.5)*GAME.tileW*5;
+		// setup origin	
+		var origin	= new CANNON.Vec3()
+		origin.set(-6*GAME.tileW, 6*GAME.tileW, -10*GAME.tileW)
+		origin.z	*= Math.floor(bodyCounter % 2) === 1 ? 1 : -1
+		origin.x	+= (Math.random()-0.5)*GAME.tileW*10;
+		origin.y	+= bodyCounter * radius*2 * 1.5
+		origin.z	+= (Math.random()-0.5)*GAME.tileW*5;
 	}
-
-	body.position.set(position.x, position.y, position.z)		
+	// init body.position to its origin
+	body.position.set(origin.x, origin.y, origin.z)
 	
 	// always be attracked by player
 	updateFcts.push(function(delta, now){
@@ -89,55 +101,34 @@ var BotBall0	= function(opts){
 		// emit a sound
 		sounds.playExplosion()
 
-		// scorePoints based on ballIntensity.intensity()
-		var scorePoints	= 10;
-		if( ballIntensity.intensity() < 0.3){
-			var scorePoints	= 100;
-		}else if( ballIntensity.intensity() < 0.6){
-			var scorePoints	= 200;
-		}else if( ballIntensity.intensity() < 0.85){
-			var scorePoints	= 600;
-		}else{
-			var scorePoints	= 1000;
-		}
+		var scorePoints	= 1000;
 		// increase score
 		yeller.dispatchEvent('increaseScore', scorePoints)
+		// increase time
+		yeller.dispatchEvent('setTime', 10.0)
 		// emit a score
 		GAME.emitterScore.emit(object3d.position, scorePoints)
 
-		this.destroy()
 		
-		var nInstances	= 0
-		scene.traverse(function(object3d){
-			nInstances	+= / ball /.test(object3d.name) ? +1 : 0
-		})
-		if( nInstances === 0 )	yeller.dispatchEvent('gameWon')
+		nLives	-= 1;
+		if( nLives > 0 ){
+			// reset all velocity
+			body.velocity.set(0,0,0)
+			body.angularVelocity.set(0,0,0)
+			// set player position
+			body.position.set(origin.x, origin.y, origin.z)		
+		}else{
+			this.destroy()
+			var nInstances	= 0
+			scene.traverse(function(object3d){
+				nInstances	+= / ball /.test(object3d.name) ? +1 : 0
+			})
+			if( nInstances === 0 )	yeller.dispatchEvent('gameWon')			
+		}
 	}.bind(this))
 
 	this.destroy	= function(){
 		physicsWorld.bodiesToRemove.push(body)
 		scene.remove( object3d )
-		scene.remove( ballIntensity.object3d )
 	}
-	
-	//////////////////////////////////////////////////////////////////////////////////
-	//		comment								//
-	//////////////////////////////////////////////////////////////////////////////////
-	
-	var ballIntensity	= new JuiceIntensityBall();	
-	scene.add( ballIntensity.object3d )
-	updateFcts.push(function(delta, now){
-		ballIntensity.update(delta, now)
-	})
-	updateFcts.push(function(delta, now){
-		ballIntensity.object3d.position.copy(object3d.position)
-	})
-	
-	// kill player if touching the goal
-	body.addEventListener("collide",function(event){
-		var collidedObj	= event.with.userData.object3d
-		var isPlayer	= / player /.test(collidedObj.name) ? true : false
-		if( !isPlayer )	return
-		ballIntensity.intensity( ballIntensity.intensity() + 0.1 )
-	})
 }
